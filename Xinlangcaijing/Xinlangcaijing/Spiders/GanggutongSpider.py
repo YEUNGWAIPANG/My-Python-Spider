7279e2557709f400670a246b6467d2c74cffcacd
YEUNGWAIPANG <2946706968@qq.com>
2022-04-03 02:40:59 +0800

    爬取新浪财经的股票信息

---
+++ Xinlangcaijing/Xinlangcaijing/Spiders/GanggutongSpider.py
@@ -0,0 +1,94 @@
+import pymysql
+import requests
+from time import sleep
+from random import randint
+from resources.Setting import *
+from resources.Pretty import Pretty
+
+class GanggutongSpider:
+    def __init__(self) -> None:
+        self.bankuai = "港股通"
+        self.tablename = self.bankuai + nowtime
+        self.link = "https://vip.stock.finance.sina.com.cn/quotes_service/api/json_v2.php/Market_Center.getHKStockData"
+        
+    def getdata(self) -> list:
+        datalist = []
+        params = Pretty().Pretty_headers("""
+            page: 1
+            num: 80
+            sort: symbol
+            asc: 1
+            node: sgt_hk
+            _s_r_a: inits
+        """)
+        params["page"] = int(params["page"])
+        while True:
+            print(self.bankuai + str(params["page"]))
+            r = requests.get(self.link,params=params)
+            r.raise_for_status()
+            if len(r.json()) == 0:
+                break
+            else:
+                for data in r.json():
+                    result = []
+                    result.append(data["symbol"])
+                    result.append(data["name"])
+                    result.append(data["lasttrade"]) #最新价
+                    result.append('%.3f'%(float(data["lasttrade"])-float(data["prevclose"])))
+                    result.append('%.3f'%((float(data["lasttrade"])-float(data["prevclose"]))/float(data["prevclose"])*100))
+                    result.append(data["prevclose"])
+                    result.append(data["open"])
+                    result.append(data["high"])
+                    result.append(data["low"])
+                    try:
+                        float(str(data["volume"])[:-4] + "." + str(data["volume"])[-4:-2])
+                        result.append(str(data["volume"])[:-4] + "." + str(data["volume"])[-4:-2])
+                    except:
+                        continue
+                    try:
+                        float(str(data["amount"])[:-4] + "." + str(data["amount"])[-4:-2])
+                        result.append(str(data["amount"])[:-4] + "." + str(data["amount"])[-4:-2])
+                    except:
+                        continue
+                    datalist.append(tuple(result))
+                params["page"] += 1
+                sleep(randint(2,5))
+        return datalist
+
+    def parse(self,datalist:list):
+        try:
+            db = pymysql.connect(host=MySQL_host,port=MySQL_port,user=MySQL_user,password=MySQL_password,database=MySQL_database)
+        except pymysql.err.OperationalError:
+            db = pymysql.connect(host=MySQL_host,port=MySQL_port,user=MySQL_user,password=MySQL_password)
+            db.cursor().execute("CREATE DATABASE %s"%MySQL_database)
+            db.commit()
+            db.close()
+            db = pymysql.connect(host=MySQL_host,port=MySQL_port,user=MySQL_user,password=MySQL_password,database=MySQL_database)
+        db.cursor().execute("""
+            CREATE TABLE %s(
+                代码 TEXT NOT NULL,
+                名称 TEXT NOT NULL,
+                最新价	FLOAT NOT NULL,
+                涨跌额	FLOAT NOT NULL, 
+                涨跌幅	FLOAT NOT NULL,
+                昨收	FLOAT NOT NULL,
+                今开	FLOAT NOT NULL,
+                最高	FLOAT NOT NULL,
+                最低	FLOAT NOT NULL,
+                成交量（万） DOUBLE NOT NULL,
+                成交额（万） DOUBLE NOT NULL
+            )
+        """%self.tablename)
+        db.commit()
+        db.cursor().executemany("INSERT INTO "+ self.tablename +" VALUES(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",datalist)
+        db.commit()
+        # for data in datalist:
+        #     try:
+        #         db.cursor().execute("INSERT INTO "+ self.tablename +" VALUES('%s','%s',%s,%s,%s,%s,%s,%s,%s,%s,%s)"%(data[0],data[1],data[2],data[3],data[4],data[5],data[6],data[7],data[8],data[9],data[10]))
+        #         db.commit()
+        #     except:
+        #         pass
+        db.close()
+
+    def run(self):
+        self.parse(self.getdata())
---
+++ Xinlangcaijing/Xinlangcaijing/resources/Setting.py
@@ -0,0 +1,8 @@
+from .Gettime import gettime  #linux下要加"."
+
+nowtime = gettime()
+MySQL_host = ""  #Mysql服务器的公网IP
+MySQL_port = 3306
+MySQL_user = "" #Mysql服务器的用户名
+MySQL_password = "" #Mysql服务器的密码
+MySQL_database = "xinlangcaijing"
