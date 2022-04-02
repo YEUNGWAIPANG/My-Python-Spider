b036f0aa946f3de7149bc3fef12db1b0e5362494
YEUNGWAIPANG <2946706968@qq.com>
2022-04-03 02:44:22 +0800

    Delete Xinlangcaijing directory

--- Xinlangcaijing/Xinlangcaijing/resources/Pretty.py
+++
@@ -1,8 +0,0 @@
-class Pretty:
-    def Pretty_headers(self,start_headers:str) -> dict:
-        headers = {}
-        start_headers = start_headers.strip().split("\n")
-        for i in start_headers:
-            head = i.strip().split(":",1)
-            headers[head[0]] = head[1].strip()
-        return headers
