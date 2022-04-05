385f37c83bbea1413bfa071aca336fb8325496b3
YEUNGWAIPANG <2946706968@qq.com>
2022-04-05 17:17:16 +0800

    阿里云云函数部署

---
+++ Dangdang/Dangdang_cloud/RunAllSpider.py
@@ -0,0 +1,19 @@
+from twisted.internet import reactor
+from scrapy.crawler import CrawlerRunner
+from scrapy.utils.log import configure_logging
+from scrapy.utils.project import get_project_settings
+from Dangdang.spiders.changxiaobang import ChangxiaobangSpider
+from Dangdang.spiders.xinshuremaibang import XinshuremaibangSpider
+from Dangdang.spiders.haopingbang import HaopingbangSpider
+
+def main(event,context):
+    configure_logging()
+    settings = get_project_settings()
+    runner = CrawlerRunner(settings)
+    runner.crawl(ChangxiaobangSpider)
+    runner.crawl(XinshuremaibangSpider)
+    runner.crawl(HaopingbangSpider)
+    d = runner.join()
+    d.addBoth(lambda _: reactor.stop())
+
+    reactor.run()
